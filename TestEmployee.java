4199ab4ce67555462c07e9e203cea4041b8976d6
narayanchonde <vishalchonde10@gmail.com>
2023-02-21 17:33:22 +0530

    Line added by me

---
+++ TestEmployee.java
@@ -0,0 +1,28 @@
+package ntech.com.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class TestEmployee {
+
+	
+	///test method
+	
+	@Test
+	public void testSave() {
+		System.out.println("hello form test ..");
+	}
+	
+	@Test
+	@AfterEach
+	public void testGetAll() {
+		System.out.println("after each");
+	}
+	
+	
+	public void testDriver() {
+		System.out.println("driver Tested ...");
+	}
+	
+	/// line added by mee from 
+}
